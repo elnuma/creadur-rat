158a9e4f70e4a497d5c4fe6ecb03f169a1618f41
Manuel Suárez Sánchez <ssm767@gmail.com>
2013-07-08 17:48:58 +0200

    Start TDD project Apache-Rat-Core

---
+++ apache-rat-core/src/main/java/org/apache/rat/document/MockLocation.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ */ 
+package org.apache.rat.document;
+
+import org.apache.rat.api.Document;
+import org.apache.rat.api.MetaData;
+/**
+ * 
+ * @author Manuel
+ *
+ *Mock class that simulate the behaviour of Document class.
+ */
+public class MockLocation implements Document {
+
+    private String name;
+    private String url;
+    private MetaData metaData = new MetaData();
+    
+    /**
+     * 
+     * @param name
+     */
+    public MockLocation(String name) {
+        this(name, "url");
+    }
+
+    /**
+     * 
+     * @param name
+     * @param url
+     */
+    public MockLocation(String name, String url) {
+        super();
+        this.name = name;
+        this.url = url;
+    }
+
+    /**
+     * 
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getURL() {
+        return url;
+    }
+
+    /**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+    /**
+     * 
+     */
+    public MetaData getMetaData() {
+        return metaData;
+    }
+
+	/**
+	 * @param metaData the metaData to set
+	 */
+	public void setMetaData(MetaData metaData) {
+		this.metaData = metaData;
+	}
+}
